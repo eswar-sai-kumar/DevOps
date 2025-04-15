f673a1da6c404616a3d5e635af9d98564ec89e24
sai kumar lakkimsetti <eswarsaikumar2@gmail.com>
2025-04-15 14:53:32 +0530

    data-source

--- 5-terraform/data-source/ec2.tf
+++ 5-terraform/data-source/ec2.tf
@@ -1,7 +1,6 @@
 resource "aws_instance" "db" {
-
     ami = data.aws_ami.ami_id.id
-    vpc_security_group_ids = ["sg-0fea5e49e962e81c9" ]
+    vpc_security_group_ids = ["sg-0f08a3ce729bd0d43" ]
     instance_type = "t3.micro"
     tags = {
         Name = "data-source-practice"
