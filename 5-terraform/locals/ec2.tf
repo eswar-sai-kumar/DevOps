3adf441ee5f1c7f498b136a4b7bccae64ce4084c
sai kumar lakkimsetti <eswarsaikumar2@gmail.com>
2025-04-15 14:19:09 +0530

    locals

--- 5-terraform/locals/ec2.tf
+++ 5-terraform/locals/ec2.tf
@@ -1,11 +1,6 @@
-# resource <resource-type> <resource-name>
-resource "aws_instance" "db" {
-
+resource "aws_instance" "db"{
     ami = local.ami_id
-    vpc_security_group_ids = [local.sg_id]
+    vpc_security_group_ids = [local.sg_id]      
     instance_type = local.instance_type
     tags = local.tags
 }
-
-
-
--- 5-terraform/locals/locals.tf
+++ 5-terraform/locals/locals.tf
@@ -1,8 +1,6 @@
-locals {
-
-  ami_id = "ami-090252cbe067a9e58"
-  sg_id = "sg-0fea5e49e962e81c9" # replace with your SG ID
-  #instance_type = "t3.micro"
+locals{
+  ami_id = "ami-09c813fb71547fc4f"
+  sg_id = "sg-0f08a3ce729bd0d43" 
   instance_type = var.instance_name == "db" ? "t3.small" : "t3.micro"
   tags = {
     Name = "locals"
