c73ff74c0b2029e82438105174b8d978568027cf
sai kumar lakkimsetti <eswarsaikumar2@gmail.com>
2025-04-15 11:47:14 +0530

    condtions

--- 5-terraform/conditions/variables.tf
+++ 5-terraform/conditions/variables.tf
@@ -1,14 +1,12 @@
-variable "image_id" {
-  type        = string #optional
-  default     = "ami-09c813fb71547fc4f" #optional
-  description = "RHEL-9 AMI ID" #optional
+variable "image_id"{
+    type = string
+    default = "ami-09c813fb71547fc4f"
+    description = "RHEL-9 AMI ID"
 }
-
-variable "instance_type" {
+variable instance_type{
     default = "t3.micro"
     type = string
 }
-
-variable "instance_name" {
+variable instance_name{
     default = "backend"
 }
