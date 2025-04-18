24e8f878f2ad471d3ee964f13cac2e2b489d8090
sai kumar lakkimsetti <eswarsaikumar2@gmail.com>
2025-04-18 19:51:34 +0530

    locals

--- 5-terraform/locals/locals.tf
+++ 5-terraform/locals/locals.tf
@@ -1,7 +1,9 @@
 locals{
   ami_id = "ami-09c813fb71547fc4f"
   sg_id = "sg-0f08a3ce729bd0d43" 
-  instance_type = var.instance_name == "db" ? "t3.small" : "t3.micro"
+  instance_type = var.instance_name == "db" ? "t3.small" : "t3.micro"  
+  # locals.tf and variables.tf both are same. Both refers key value pairs
+  # We can use variables inside locals, but you can't use other variables/locals inside variables
   tags = {
     Name = "locals"
   }
