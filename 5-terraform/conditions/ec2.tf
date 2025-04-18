334b09a9952e6302bb8a11a4e2f86ecd5bfa443c
sai kumar lakkimsetti <eswarsaikumar2@gmail.com>
2025-04-18 19:11:54 +0530

    conditions

--- 5-terraform/conditions/ec2.tf
+++ 5-terraform/conditions/ec2.tf
@@ -1,4 +1,4 @@
 resource "aws_instance" "db"{
     ami = var.image_id
-    instance_type = var.instance_name == "db" ? "t3.small":"t3.micro"
+    instance_type = var.instance_name == "db" ? "t3.small":"t3.micro" # if instance name is db, it will take t3.small, otherwise t3.micro
 }
