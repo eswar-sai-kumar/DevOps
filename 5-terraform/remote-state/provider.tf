48a4e5e85aec255f0c8d0ff6c64b62d569740d34
sai kumar lakkimsetti <eswarsaikumar2@gmail.com>
2025-04-18 20:07:10 +0530

    remote-state

--- 5-terraform/remote-state/provider.tf
+++ 5-terraform/remote-state/provider.tf
@@ -5,11 +5,17 @@ terraform {
       version = "5.48.0"
     }
   }
+  # In terraform there is remote state. It does not allow duplicate resources to be created
+  # In remote state .lock , .tfstate file will be present
+  # .lock file == When one person creating infra. It should be locked until it gets completed
+  # .tfstate == this file tracks actual infra. It checks infra you are creating already present or not in .tfstate file. Nothing but refreshing state.
+  # for storing .tf state file : S3 bucked created
+  # for locking : dynamoDB created
   backend "s3" {
-    bucket = "daws78s-remote-state"
+    bucket = "daws78s-remote-state"     # name of the bucket
     key    = "remote-state-demo"
     region = "us-east-1"
-    dynamodb_table = "daws78s-locking"
+    dynamodb_table = "daws78s-locking"   # name of the table
   }
 }
 
